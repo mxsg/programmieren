47ca7ff76645fcfcf458a3ce44b669b0c0573314
Maximilian Stemmer-Grabow <dev@mxsg.de>
2014-06-05 22:34:22 +0200

    Lösung für Aufgabe 20 hinzufügen

---
+++ blatt08/a20-primfaktor-2.cpp
@@ -0,0 +1,28 @@
+/*
+ * Programmieren fuer Physiker: C++
+ * SS 2014
+ *
+ * Aufgabe 20
+ * Wie oft durch zwei teilbar?
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int primeFactorTwo(int value) {
+
+    return (!(value % 2))? primeFactorTwo(value/2) + 1 : 0;
+}
+
+int main() {
+
+    // Zahl einlesen
+    int value;
+    cout << "Bitte eine Zahl eingeben: ";
+    cin >> value;
+
+    cout << "Anzahl des Primfaktors 2 bei dieser Zahl: " << primeFactorTwo(value) << endl;
+
+    return 0;
+}
