70ecd79643e8e0dc9afd6411af09308aeca3bd8a
Maximilian Stemmer-Grabow <dev@mxsg.de>
2014-06-13 15:13:13 +0200

    std-Namespace verwenden, explizite Angabe entfernen

--- blatt09/rectangle/rect-oop.cpp
+++ blatt09/rectangle/rect-oop.cpp
@@ -9,23 +9,23 @@
 #include <iostream>
 #include "Rectangle.h"
 
-//using namespace std;
+using namespace std;
 
 int main() {
 
-    std::cout << "Rechteck-Klasse" << std::endl;
+    cout << "Rechteck-Klasse" << endl;
     Rectangle r1(3.3, 2.0);
     Rectangle r2(1.8, 2.7);
     
-    std::cout << "Vor Rotation: ";
-    if(r1.covers(r2)) std::cout << "r1 bedeckt r2.";
-    else std::cout << "r1 bedeckt r2 nicht.";
-    std::cout << std::endl;
+    cout << "Vor Rotation: ";
+    if(r1.covers(r2)) cout << "r1 bedeckt r2.";
+    else cout << "r1 bedeckt r2 nicht.";
+    cout << endl;
     
     r2.rotate();
     
-    std::cout << "Nach Rotation: ";
-    if(r1.covers(r2)) std::cout << "r1 bedeckt r2.";
-    else std::cout << "r1 bedeckt r2 nicht.";
-    std::cout << std::endl;
+    cout << "Nach Rotation: ";
+    if(r1.covers(r2)) cout << "r1 bedeckt r2.";
+    else cout << "r1 bedeckt r2 nicht.";
+    cout << endl;
 }
